9332b453abb93ad6da23b8c96af4a41300bf90f6
TingYao Hsu <tingyaohsu@tutorabc.com>
2016-11-27 22:01:31 +0800

    Add scheduler

--- index.js
+++ index.js
@@ -1,21 +1,54 @@
+var schedule = require('node-schedule');
+var IncomingWebhook = require('@slack/client').IncomingWebhook;
 var webdriverio = require('webdriverio');
 var options = {
     desiredCapabilities: {
         browserName: 'chrome'
     }
 };
-webdriverio
-    .remote(options)
-    .init()
-    .url('http://tpehrweb.tutorabc.com/TIMG_inout/form/index.html')
-    .getTitle().then(function(title) {
-        console.log('Title was: ' + title);
-        //this.waitForSelector('input[name="employeeid"]');
-    })
-    .setValue('input[name="employeeid"]', '104012101421')
-    .click('#btn_cardNumber')
-    .saveScreenshot('./step1.png')
-    .click('#check_in')
-    // .click('#check_out')
-    .saveScreenshot('./step2.png')
-    .end();
+
+var url = process.env.SLACK_WEBHOOK_URL || 'https://hooks.slack.com/services/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'; //see section above on sensitive data
+
+var webhook = new IncomingWebhook(url);
+
+
+var task = function() {
+    console.log('Starting...');
+    webdriverio
+        .remote(options)
+        .init()
+        .url('http://tpehrweb.tutorabc.com/TIMG_inout/form/index.html')
+        .getTitle().then(function(title) {
+            console.log('Title was: ' + title);
+            //this.waitForSelector('input[name="employeeid"]');
+        })
+        .setValue('input[name="employeeid"]', '104012101421')
+        .click('#btn_cardNumber')
+        .saveScreenshot('./step1.png')
+        .click('#check_in')
+        // .click('#check_out')
+        .saveScreenshot('./step2.png')
+        .end();
+    webhook.send('Hello there', function(err, res) {
+        if (err) {
+            console.log('Error:', err);
+        } else {
+            console.log('Message sent: ', res);
+        }
+    });
+}
+
+var genTask = function() {
+
+    var nextDate = new Date();
+    var begin = 0;
+    var end = 59;
+    var second = Math.floor((Math.random() * Math.abs(end - begin) + 1) + begin);
+    nextDate.setSeconds(second);
+    // var minute = nextDate.getMinutes() + 1;
+    // nextDate.setMinutes(minute);
+    console.log('Job will start in...', nextDate);
+    schedule.scheduleJob(nextDate, task);
+
+};
+var j = schedule.scheduleJob('* * * * *', genTask);
